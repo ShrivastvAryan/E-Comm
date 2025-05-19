a4ba65b12d3148e2dac080fe9628a30a7264dffe
ShrivastvAryan <asguy29@gmail.com>
2025-05-19 23:56:02 +0530

    updated

--- frontend/src/app/ui/filter.jsx
+++ frontend/src/app/ui/filter.jsx
@@ -1,21 +1,34 @@
 'use client'
-import { Select } from "@chakra-ui/react"
+import { Select,Button } from "@chakra-ui/react"
 
 import React from "react"
 
 const Filter=()=>{
     return(
         <>
-        <div className="w-screen h-16 flex justify-center items-center">
-            <div className="w-[90vw] h-10 bg-red-500 rounded-md flex flex-row">
+        <div className="w-screen h-auto flex justify-center items-center pt-4 pb-1">
+            <div className="w-[90vw] h-auto rounded-md flex flex-col ">
 
-                <div className="h-10 w-[30%] p-2 flex flex-row">Price
-                    <div>
-                    <Select placeholder='Set by Price'>
+                <div className="h-auto w-full p-2 flex flex-row">Price:
+                    <div className="w-full ml-2 bo">
+                    <Select  size='xs' className="rounded-lg bg-blue-300">
                     <option value='option1'>High to Low</option>
                     <option value='option2'>Low to High</option>
                     </Select>
                     </div>
+                </div>
+
+                <div className="h-auto w-full p-2 flex flex-row">Popularity:
+                    <div className="w-full pl-2">
+                    <Select  size='xs' className="rounded-lg bg-blue-300">
+                    <option value='option1'>High to Low</option>
+                    <option value='option2'>Low to High</option>
+                    </Select>
+                    </div>
+                </div>
+
+                <div className="p-2">
+                <Button size='xs' className="bg-blue-400" >Reset</Button>
                 </div>
             </div>
 
