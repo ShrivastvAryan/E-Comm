4b6dc5af400ab5584dfed6373223388dc53a1fcf
ShrivastvAryan <asguy29@gmail.com>
2025-06-17 14:01:59 +0530

    updated

--- admin/src/app/productlist/page.jsx
+++ admin/src/app/productlist/page.jsx
@@ -26,33 +26,31 @@ const AllProduct=()=>{
         <Sidebar/>
         </div>
 
-        <div className="h-screen w-[80%] bg-slate-400">
+        <div className="h-screen w-[80%] bg-slate-200 ">
 
         <div className="h-auto w-auto p-5">
-        <h1 className="text-2xl font-semibold">All Products</h1>
-        <p>Products</p>
-        <p>Title</p>
-        <p>Old Price</p>
-        <p>New Price</p>
-        <p>Category</p>
-        <p></p>
-        </div>
+        <h1 className="text-3xl font-semibold pb-2">All Products</h1>
 
-        </div>
-
-        <div>
+         <div>
             {allproducts.map((product,index)=>(
-                <div key={index} >
-                    <img src={product.image} alt=""/>
-                    <p>{product.name}</p>
-                    <p>${product.old_price}</p>
-                    <p>${product.new_price}</p>
-                    <p>{product.category}</p>
+                <div key={index} className="h-auto w-auto bg-slate-100 flex justify-between shadow-md mt-4 rounded-md p-2 pt-3" >
+                    <div>
+                    <p className="text-2xl font-semibold">Product Title: {product.name}</p>
+                    <p className="text-xl mt-2">Original Price: ${product.old_price}</p>
+                    <p className="text-xl">Offer Price: ${product.new_price}</p>
+                    <p className="text-xl">Category: {product.category}</p>
                     <img src=""></img>
+                    </div>
+                    <div>
+                        <img src={product.image} alt="preview-image" className="h-40 w-40 object-contain"/>
+                    </div>
                 </div>
             ))}
         </div>
 
+        </div>
+
+        </div>
 
         </div>
         </>
